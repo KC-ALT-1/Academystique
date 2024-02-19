b17dd159c935ebf59304f9cb0364212ebd434a13
KC-ALT-1 <stormyplayz20@gmail.com>
2024-02-19 21:34:18 +0530

    final changes

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from sklearn.impute import SimpleImputer
 
 app = Flask(__name__)
 
-
+# Load the trained model and OneHotEncoder
 with open("random.pkl", "rb") as file:
     model = pickle.load(file)
 
+# Load the OneHotEncoder instance used during training
 with open("encoder.pkl", "rb") as file:
     encoder = pickle.load(file)
 
@@ -43,14 +44,14 @@ def home():
             float(request.form[f]) for f in ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]
         ]
 
-      
+        # Transform inputs using the OneHotEncoder
         transformed_inputs = encoder.transform([inputs]).toarray()
 
         result = model.predict(transformed_inputs)
       
         result = "{:.2f}".format(result[0])
 
-        # Format prediction output based on different scenarios
+        
         if (all(i == 0 for i in inputs[0:3])) or (inputs[5] > 5) or (inputs[8] < 40):
             if all(i == 0 for i in inputs[0:3]):
                 prediction_output = "Fail: All semesters' GPA are 0."
@@ -63,6 +64,8 @@ def home():
                 tips = []  
         else:
             prediction_output = "Predicted GPA for {}: {}".format(name, result)
+            
+            
             subject = request.form.get("subject")  
             tips = subject_tips.get(subject, [])
 
