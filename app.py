0cd2feeb94e6ffc0bf367b003cd4492c86ce9b7a
KC-ALT-1 <stormyplayz20@gmail.com>
2024-02-19 21:00:09 +0530

    final changes

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ from sklearn.impute import SimpleImputer
 
 app = Flask(__name__)
 
-# Load the trained model and OneHotEncoder
+
 with open("random.pkl", "rb") as file:
     model = pickle.load(file)
 
-# Load the OneHotEncoder instance used during training
 with open("encoder.pkl", "rb") as file:
     encoder = pickle.load(file)
 
@@ -44,7 +43,7 @@ def home():
             float(request.form[f]) for f in ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]
         ]
 
-        # Transform inputs using the OneHotEncoder
+      
         transformed_inputs = encoder.transform([inputs]).toarray()
 
         result = model.predict(transformed_inputs)
@@ -55,18 +54,16 @@ def home():
         if (all(i == 0 for i in inputs[0:3])) or (inputs[5] > 5) or (inputs[8] < 40):
             if all(i == 0 for i in inputs[0:3]):
                 prediction_output = "Fail: All semesters' GPA are 0."
-                tips = []  # Define an empty list for tips
+                tips = []  
             elif inputs[5] > 5:
                 prediction_output = "Drop: Backlogs greater than 5."
-                tips = []  # Define an empty list for tips
+                tips = []  
             else:
                 prediction_output = "Expected Pointer less than 4: IQ less than 40."
-                tips = []  # Define an empty list for tips
+                tips = []  
         else:
             prediction_output = "Predicted GPA for {}: {}".format(name, result)
-            
-            # Fetch tips based on the selected subject
-            subject = request.form.get("subject")  # Use request.form.get to avoid KeyError
+            subject = request.form.get("subject")  
             tips = subject_tips.get(subject, [])
 
         return render_template("form.html", abcc=prediction_output, tips=tips)
